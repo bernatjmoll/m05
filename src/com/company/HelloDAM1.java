8a2e9fccb2839f1825e285336a8ba3e83ea87dc1
Bernat <berantjuanedamoll@gmai.com>
2022-01-21 10:53:02 +0100

    comit bernat

--- src/com/company/HelloDAM1.java
+++ src/com/company/HelloDAM1.java
@@ -5,5 +5,6 @@ public class HelloDAM1 {
     public static void main(String[] args) {
         System.out.println("MD05-activitatGit");
         System.out.println("Josep Coma :-)");
+        System.out.println("Bernat Juaneda");
     }
 }
